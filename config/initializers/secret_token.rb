7ad2259421e4ba1abef9b440dd9e8dc9fd983a25
Naveen Chandran <naveen@logicgears.com>
2010-10-12 15:52:34 +0530

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exceldb::Application.config.secret_token = '5b16f2b8fa508d719ca376713dde9ee587e359dc42df9a00d4d25b4eb031b90f77d0d76a6471e405a6079fe167432d779fc9523158f800d7512f280664c10ea9'
